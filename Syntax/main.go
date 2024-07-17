51b85985932bd5160562a119ff378d0936bf1a3c
huynguyen <tronghuy0077@gmail.com>
2024-07-17 11:33:40 +0700

    variable

--- Syntax/main.go
+++ Syntax/main.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("go" + "lang")
-	fmt.Println("1+1 =", 1+1)
-	fmt.Println("7.0/3.0 =", 7.0/3.0)
-	fmt.Println(true && false)
-	fmt.Println(true || false)
-	fmt.Println(!true)
+	var a = "inittial"
+	fmt.Println(a)
+
+	var b, c = 1, 2
+	fmt.Println(b, c)
+	var d = true
+	fmt.Println(d)
+	var e int
+	fmt.Println(e)
+	f := "apple"
+	fmt.Println(f)
+
 }
