2711462a5469c77b7d97a598f99e084e3accd73a
huynguyen <tronghuy0077@gmail.com>
2024-07-17 11:37:16 +0700

    constants

--- Syntax/main.go
+++ Syntax/main.go
@@ -1,18 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+const s = "constant"
 
 func main() {
-	var a = "inittial"
-	fmt.Println(a)
-
-	var b, c = 1, 2
-	fmt.Println(b, c)
-	var d = true
+	fmt.Println(s)
+	const n = 500000000
+	const d = 3e20 / n
 	fmt.Println(d)
-	var e int
-	fmt.Println(e)
-	f := "apple"
-	fmt.Println(f)
+	fmt.Println(int64(d))
+	fmt.Println(math.Sin(n))
 
 }
